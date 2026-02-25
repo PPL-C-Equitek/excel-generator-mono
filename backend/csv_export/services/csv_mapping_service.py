1b110d0112be0d458ab4ac6f45ef9a3185a509a0
zufarra <zufarromli@gmail.com>
2026-02-25 22:07:07 +0700

    [GREEN]: Handle value error when mapping header are empty in CSV Mapping Service.

--- backend/csv_export/services/csv_mapping_service.py
+++ backend/csv_export/services/csv_mapping_service.py
@@ -1,3 +1,6 @@
 class CSVMappingService:
     def map_rows(self, headers, rows):
+        if not headers:
+            raise ValueError("headers must not be empty")
+
         return [[row[header] for header in headers] for row in rows]
