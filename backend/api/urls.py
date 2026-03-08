8f1760cc960ff07a712eedfd690bcbfcad585dcc
arishashaista <arishashaistaa@gmail.com>
2026-03-08 23:52:20 +0700

    REFACTOR: lint urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('health/', views.health),
-    path('about/', views.about),
-    path('members/', views.members),
-    path('upload/', views.upload),
-    path('export/csv', views.export_csv),
-    path('export/csv/<str:file_id>/download', views.download_csv),
+    path("health/", views.health),
+    path("about/", views.about),
+    path("members/", views.members),
+    path("upload/", views.upload),
+    path("export/csv", views.export_csv),
+    path("export/csv/<str:file_id>/download", views.download_csv),
 ]
