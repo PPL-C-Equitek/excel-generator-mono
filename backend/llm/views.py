2003626f88886359a1e503e286e67ece5913b727
Steven Setiawan <stevensetiawan1802@gmail.com>
2026-03-11 21:07:17 +0700

    [REFACTOR] Fix lint

--- backend/llm/views.py
+++ backend/llm/views.py
@@ -4,7 +4,6 @@ from django.views.decorators.http import require_http_methods
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.decorators import rate_limit
 from .serializers import LlmGenerateRequestSerializer, LlmGenerateResponseSerializer
 from .services.openai_client import (
     OpenAIConfigurationError,
