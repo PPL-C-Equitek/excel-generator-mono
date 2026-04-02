27f94ffc8505a0cf7c71629d36be6efcd79d73b8
naylafn <naylafarah.31@gmail.com>
2026-04-02 09:58:23 +0700

    [FEAT] Implement handleLogin in LoginPage with FetchAPI

--- frontend/src/app/login/LoginPage.tsx
+++ frontend/src/app/login/LoginPage.tsx
@@ -1,16 +1,29 @@
+'use client'
 import Navbar from '@/components/Navbar'
 import LoginForm from '@/components/LoginForm'
 import { LANDING_NAV_LINKS } from '@/constants/landing'
+import type { LoginFormData } from '@/components/LoginForm'
+import { login } from '@/lib/api'
 
 export default function LoginPage() {
+    const handleLogin = async (data: LoginFormData) => {
+        try {
+            const res = await login(data.email, data.password)
+
+            localStorage.setItem('access_token', res.access_token)
+            localStorage.setItem('refresh_token', res.refresh_token)
+
+            window.location.href = '/convert'
+        } catch (err: any) {
+            alert(err.message)
+        }
+    }
+
     return (
-        <div
-            className="force-light min-h-screen flex flex-col"
-            style={{ backgroundColor: 'var(--surface-1)', colorScheme: 'light' }}
-        >
+        <div className="force-light min-h-screen flex flex-col">
             <Navbar links={LANDING_NAV_LINKS} activePage="login" />
             <main className="flex flex-1 items-center justify-center px-4 py-12">
-                <LoginForm />
+                <LoginForm onSubmit={handleLogin} />
             </main>
         </div>
     )
