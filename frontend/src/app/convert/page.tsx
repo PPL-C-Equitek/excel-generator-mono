2a2e022e4ecccce7c0a17623757b6b0b0e8af410
naylafn <naylafarah.31@gmail.com>
2026-04-05 19:53:39 +0700

    [REFACTOR] use withProtectedPage on convert page

--- frontend/src/app/convert/page.tsx
+++ frontend/src/app/convert/page.tsx
@@ -1,53 +1,6 @@
 "use client"
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
 import ConvertPage from './ConvertPage'
-import { getValidAccessToken } from '@/lib/auth'
+import { withProtectedPage } from '@/lib/withProtectedPage'
 
-// Debug function
-// function parseJwt(token: string) {
-//     try {
-//         const base64Payload = token.split('.')[1]
-//         const payload = atob(base64Payload)
-//         return JSON.parse(payload)
-//     } catch {
-//         return null
-//     }
-// }
-
-export default function Page() {
-    const router = useRouter()
-    const [isCheckingAuth, setIsCheckingAuth] = useState(true)
-
-    useEffect(() => {
-        let isCancelled = false
-
-        const checkAuth = async () => {
-            const accessToken = await getValidAccessToken()
-
-            if (isCancelled) {
-                return
-            }
-
-            if (!accessToken) {
-                router.replace('/login')
-                return
-            }
-
-            setIsCheckingAuth(false)
-        }
-
-        void checkAuth()
-
-        return () => {
-            isCancelled = true
-        }
-    }, [router])
-
-    if (isCheckingAuth) {
-        return null
-    }
-
-    return <ConvertPage />
-}
+export default withProtectedPage(ConvertPage)
