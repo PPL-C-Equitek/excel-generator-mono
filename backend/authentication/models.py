738cf183eac8aa01fc961c3359d17a50aa65b28a
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2026-04-13 13:53:53 +0700

    [GREEN] implement explicit token expiration validation and secure set password logic

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -22,6 +22,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
     status = models.CharField(max_length=20, default="unverified")
+    email_verification_nonce = models.UUIDField(default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     objects = UserManager()
